bc1b48fc21713bd307989fdf4a7d78de69119d63
Antonin Carlin <antonin.carlin@icloud.com>
2021-12-17 11:24:06 +0100

    feat: new exercices

---
+++ Exercice_4/script.js
@@ -0,0 +1,63 @@
+/**
+ * Les types
+ * Trouve le type de chaque element du tableau
+ */
+
+const types = [
+  "alex",     // type ??
+  10,         // type ??
+  true,       // type ??
+  [1, 2, 3],  // type ??
+  {},         // type ??
+  false,      // type ??
+  document.createElement("h1"), // type ??
+]
+
+
+
+/**
+ * Les boucles
+ */
+// ex1 ---------------------------------------------------------------------------------
+// affiche dans la console tous les nombres de 0 à 10 
+// aide : https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Loops_and_iteration
+
+for (????) {
+  console.log(????)
+}
+
+
+// ex2 ---------------------------------------------------------------------------------
+// affiche chaque element du tableau dans la console
+// aide : https://developer.mozilla.org/fr/docs/Web/JavaScript/Guide/Loops_and_iteration
+// aide : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+
+const mcaNames = ["Marc", "Valerie", "Alex", "Cyril", "Jennifer", "Iris"];
+
+console.log(?????)
+
+
+/**
+ * Les functions
+ */
+
+// ex1 ---------------------------------------------------------------------------------
+// créer une fonction qui prend en paramètre un nombre et qui le multiplie par 2
+// aide : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
+
+function muliplyByTwo(????) {
+  ????
+}
+
+console.log(multiplyByTwo(10))
+
+
+// ex2 ---------------------------------------------------------------------------------
+// créer une fonction qui prend en paramètre un nom et un prénom et qui retourne "Bonjour, prénom nom"
+// aide : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
+
+function sayHelloTo(????) {
+  ????
+}
+
+console.log(sayHelloTo("Tausky", "Alexendre"))
